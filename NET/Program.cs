249a7a5cf8350fd1a6edfb10ee41419e01cc2e01
~Neo <300kghrak@gmail.com>
2026-07-04 13:13:45 +0400

    intitial commit

--- NET/Program.cs
+++ NET/Program.cs
@@ -1,230 +1,54 @@
-﻿abstract class Device
+﻿using System.Runtime.CompilerServices;
+
+interface IOutput
 {
-    public string? Name { get; protected set; }
-    public string? Description { get; protected set; }
-    protected Device(string? name, string? description)
-    {
-        Name = name;
-        Description = description;
-    }
-
-    public virtual void Sound() { }
-    public virtual void Show() { }
-    public virtual void Desc() { }
-}
-
-class Kettle : Device
-{
-    public Kettle(string? name, string? description) : base(name, description)
-    {
-    }
-    public override void Sound()
-    {
-        Console.WriteLine("Kettle is boiling water...");
-    }
-    public override void Show()
-    {
-        Console.WriteLine($"Kettle Name: {Name}");
-    }
-    public override void Desc()
-    {
-        Console.WriteLine($"Kettle Description: {Description}");
-    }
-}
-class Microwave : Device
-{
-    public Microwave(string? name, string? description) : base(name, description)
-    {
-    }
-    public override void Sound()
-    {
-        Console.WriteLine("Microwave is heating food...");
-    }
-
-    public override void Show()
-    {
-        Console.WriteLine($"Microwave Name: {Name}");
-    }
-
-    public override void Desc()
-    {
-        Console.WriteLine($"Microwave Description: {Description}");
-    }
-}
-
-class Automobile : Device
-{
-    public Automobile(string? name, string? description) : base(name, description)
-    {
-    }
-
-    public override void Sound()
-    {
-        Console.WriteLine("Automobile is moving...");
-    }
-    public override void Show()
-    {
-        Console.WriteLine($"Automobile Name: {Name}");
-    }
-    public override void Desc()
-    {
-        Console.WriteLine($"Automobile Description: {Description}");
-    }
+    void Show();
+    void Show(string info);
 }
 
 
-class Steamship : Device
+class MyArray: IOutput
 {
-    public Steamship(string? name, string? description) : base(name, description)
+    public int[] array;
+    public MyArray()
     {
+        array = new int[0];
     }
-    public override void Sound()
+
+    public MyArray(int[] array)
     {
-        Console.WriteLine("Steamship is sailing...");
+        this.array = new int[array.Length];
+        for (int i = 0; i < array.Length; i++)
+        {
+            this.array[i] = array[i];
+        }
     }
-    public override void Show()
+
+    public void Show()
     {
-        Console.WriteLine($"Steamship Name: {Name}");
+        for (int i = 0; i <  array.Length; i++)
+        {
+           Console.WriteLine(array[i]);
+        }
     }
-    public override void Desc()
+
+    public void Show(string info)
     {
-        Console.WriteLine($"Steamship Description: {Description}");
+        Console.WriteLine(info);
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
     }
 }
-
-
-#region Musical Instruments
-
-abstract class MusicalInstrument
-{
-    public string? Name { get; protected set; }
-    public string? Description { get; protected set; }
-    public string? History { get; protected set; }
-
-    protected MusicalInstrument(string? name, string? description, string? history)
-    {
-        Name = name;
-        Description = description;
-        History = history;  
-    }
-
-    public virtual void Sound() { }
-    public virtual void Show() { }
-    public virtual void Desc() { }
-    public virtual void Hist() { }
-}
-
-class Violin : MusicalInstrument
-{
-    public Violin(string? name, string? description, string? history) : base(name, description, history)
-    {
-    }
-
-    public override void Sound()
-    {
-        Console.WriteLine("Violin is playing...");
-    }
-
-    public override void Show()
-    {
-        Console.WriteLine($"Violin Name: {Name}");
-    }
-
-    public override void Desc()
-    {
-        Console.WriteLine($"Violin Description: {Description}");
-    }
-    public override void Hist() {
-        Console.WriteLine($"Violin History: {History}");
-    }
-}
-
-class Trombone : MusicalInstrument
-{
-    public Trombone(string? name, string? description, string? history) : base(name, description, history)
-    {
-    }
-
-    public override void Sound()
-    {
-        Console.WriteLine("Trombone is playing...");
-    }
-
-    public override void Show()
-    {
-        Console.WriteLine($"Trombone Name: {Name}");
-    }
-
-    public override void Desc()
-    {
-        Console.WriteLine($"Trombone Description: {Description}");
-    }
-
-    public override void Hist()
-    {
-        Console.WriteLine($"Trombone History: {History}");
-    }
-}
-
-class Ukulele : MusicalInstrument
-{
-    public Ukulele(string? name, string? description, string? history) : base(name, description, history)
-    {
-    }
-
-    public override void Sound()
-    {
-        Console.WriteLine("Ukulele is playing...");
-    }
-
-    public override void Show()
-    {
-        Console.WriteLine($"Ukulele Name: {Name}");
-    }
-
-    public override void Desc()
-    {
-        Console.WriteLine($"Ukulele Description: {Description}");
-    }
-
-    public override void Hist()
-    {
-        Console.WriteLine($"Ukulele History: {History}");
-    }
-}
-
-class Cello : MusicalInstrument
-{
-    public Cello(string? name, string? description, string? history) : base(name, description, history)
-    {
-    }
-
-    public override void Sound()
-    {
-        Console.WriteLine("Cello is playing...");
-    }
-
-    public override void Show()
-    {
-        Console.WriteLine($"Cello Name: {Name}");
-    }
-
-    public override void Desc()
-    {
-        Console.WriteLine($"Cello Description: {Description}");
-    }
-    
-    public override void Hist()
-    {
-        Console.WriteLine($"Cello History: {History}");
-    }
-}
-
-#endregion
 
 class Program
 {
     static void Main()
     {
+        int[] arr = {1, 2, 3, 4, 5};
+        MyArray array = new MyArray(arr);
+        array.Show();
+        array.Show("Array: ");
     }
 }
